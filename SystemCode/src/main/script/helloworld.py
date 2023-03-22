2697502753ed1b82a3a61bb50fc1f3cac375aa0e
Borromeo Angelie Quiapo <angelieqborromeo@gmail.com>
2023-03-22 18:29:23 +0800

    Initial Commit

---
+++ SystemCode/src/main/script/helloworld.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 12 15:51:59 2023
+
+@author: aqborromeo
+"""
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+
+
+from tensorflow import keras
+#from keras.models import Sequential
+#from keras.layers import Dense, Activation, Dropout
+#from keras.optimizers import SGD
+#import random
+
+
+
+print("Hello World")
